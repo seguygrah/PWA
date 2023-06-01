d0c73d2c5fbb1d80730b7ca430cac6bfe909bbc7
seguygrah <124881804+seguygrah@users.noreply.github.com>
2023-06-01 16:38:10 +0400

    hh

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ self.addEventListener('install', (e) =>
 {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open (cacheName).then ((cache) =>
+        caches.open (cacheName).then((cache) =>
         {
             console.log ('[Service Worker] Caching app shell');
             return cache.addAll(cacheFiles);

389ea1adb37df8e6ad582aae1118a3a84961314d
seguygrah <124881804+seguygrah@users.noreply.github.com>
2023-06-01 16:26:39 +0400

    second com

--- service-worker.js
+++ service-worker.js
@@ -23,16 +23,34 @@ self.addEventListener('install', (e) =>
     )
 });
 
+// self.addEventListener('fetch', function (e)
+// {
+//     e.respondWith 
+// (
+//     //check if the cache has the file
+//     caches.match(e.request).then (function (r)
+//     {
+//         console.log('[Service Worker] Fecthing resource: '+ e.request.url);
+//     //'r' is the matching file if it exists in the cache
+//     return r    
+//     })
+// );
+// });
 self.addEventListener('fetch', function (e)
 {
-    e.respondWith 
-(
-    //check if the cache has the file
-    caches.match(e.request).then (function (r)
-    {
-        console.log('[Service Worker] Fecthing resource: '+ e.request.url);
-    //'r' is the matching file if it exists in the cache
-    return r    
-    })
-);
+    e.respondWith(
+        caches.match(e.request).then(function (r)
+        {
+            //download the file if it is not in the cache,
+            return r || fetch (e.request).then(function (response)
+            {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache)
+                {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            })
+        })
+    );
 });

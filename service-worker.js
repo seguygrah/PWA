d0fd44a3d813cd0f5e97be6587f96546dfecd187
seguygrah <124881804+seguygrah@users.noreply.github.com>
2023-06-01 16:40:18 +0400

    new

--- service-worker.js
+++ service-worker.js
@@ -11,12 +11,10 @@ var cacheFiles =
     'images/icon-store-512.png'
 
 ]
-self.addEventListener('install', (e) => 
-{
+self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open (cacheName).then((cache) =>
-        {
+        caches.open (cacheName).then((cache) =>{
             console.log ('[Service Worker] Caching app shell');
             return cache.addAll(cacheFiles);
         })
